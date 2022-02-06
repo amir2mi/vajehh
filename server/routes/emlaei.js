92f195d39ebced84306f92200fb8acd9d52453ff
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-02-06 20:16:10 +0330

    Emlaei with nspell library

--- server/routes/emlaei.js
+++ server/routes/emlaei.js
@@ -1,18 +1,25 @@
 const express = require("express");
-const Typo = require("typo-js");
-const path = require("path");
 
+const path = require("path");
+var dictionary = require("dictionary-fa");
+var nspell = require("nspell");
 const router = express.Router();
 
-const dictionary = new Typo("fa_IR", false, false, { dictionaryPath: path.dirname(__dirname) + "/dictionaries" });
 
 // return result based on the given string
 router.get("/:word", (req, res) => {
   const { word } = req.params;
 
-  var suggestion = dictionary.suggest(word);
+  dictionary((err, dict) => {
+    if (err) {
+      throw err;
+    }
 
-  res.send(suggestion && suggestion);
+    const spell = nspell(dict);
+    const result = spell.suggest(word);
+  
+    res.send(result);
+  });
 });
 
 module.exports = router;
