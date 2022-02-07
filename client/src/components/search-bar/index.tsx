c27cd69df3060b82e4ae9b16181fe6baeefbdb7d
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-02-07 18:43:38 +0330

    Disabled cspell and created tab and search bar files

---
+++ client/src/components/search-bar/index.tsx
@@ -0,0 +1,10 @@
+import { Input } from "react-flatifycss";
+
+function SearchBar(){
+
+    return (
+        <Input>
+         <button className="search-button" aria-label="جستجو"></button>
+        </Input>
+    )
+}
