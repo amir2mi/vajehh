fe854a5aa402e24e1a7701bc06eff7c3ba411686
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-02-09 16:41:01 +0330

    Added Vazir typeface

--- client/src/components/DictionaryPicker/index.tsx
+++ client/src/components/DictionaryPicker/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Dropdown } from 'react-flatifycss';
 
 export default function DictionaryPicker(){
-    
+    return (
+       <Dropdown buttonLabel="منابع جستجو"></Dropdown>
+
+    )
 }
