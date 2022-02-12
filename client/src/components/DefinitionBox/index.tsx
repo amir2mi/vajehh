78129c8ba614b4a15c0b81b371f473f29529b075
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-02-13 00:46:37 +0330

    Added highlighting feature to definition box

--- client/src/components/DefinitionBox/index.tsx
+++ client/src/components/DefinitionBox/index.tsx
@@ -1,20 +1,32 @@
+import Highlighter from "react-highlight-words";
 import "./style.scss";
 
 interface DefinitionBoxProps {
   children?: string | React.ReactNode;
   definition?: string;
+  highlight?: string[] | false;
   title: string;
   titleTagName?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 }
 
 export default function DefinitionBox(props: DefinitionBoxProps) {
-  const { children, titleTagName, definition, title } = props;
+  const { children, definition, highlight, title, titleTagName } = props;
   const Heading = titleTagName || "h2";
 
   return (
     <div className="definition-box">
       <Heading className="definition-title">{title}</Heading>
-      {definition && <p className="definition">{definition}</p>}
+      {highlight ? (
+        <Highlighter
+          className="definition"
+          highlightClassName="marked-word style-yellow-light size-sm"
+          autoEscape={true}
+          searchWords={highlight}
+          textToHighlight={definition}
+        />
+      ) : (
+        <p className="definition">{definition}</p>
+      )}
       {children}
     </div>
   );
