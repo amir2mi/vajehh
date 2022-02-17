06d3b8d45109be3d33b958a748a1043a3dc93f4e
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-02-17 20:59:48 +0330

    Added dictionaries icons

--- client/src/components/Icons/index.tsx
+++ client/src/components/Icons/index.tsx
@@ -1,12 +1,25 @@
-import { ReactComponent as FourColumns } from "../../assets/svg/four-columns.svg";
-import { ReactComponent as OneColumn } from "../../assets/svg/one-column.svg";
-import { ReactComponent as ThreeColumns } from "../../assets/svg/three-columns.svg";
-import { ReactComponent as TwoColumns } from "../../assets/svg/two-columns.svg";
+// columns
+import { ReactComponent as FourColumns } from "../../assets/svg/columns/four-columns.svg";
+import { ReactComponent as OneColumn } from "../../assets/svg/columns/one-column.svg";
+import { ReactComponent as ThreeColumns } from "../../assets/svg/columns/three-columns.svg";
+import { ReactComponent as TwoColumns } from "../../assets/svg/columns/two-columns.svg";
+
+// dictionaries icon
+import { ReactComponent as IntroMotaradef } from "../../assets/svg/dics/motaradef.svg";
+import { ReactComponent as IntroSereh } from "../../assets/svg/dics/sereh.svg";
+import { ReactComponent as IntroTeyfi } from "../../assets/svg/dics/teyfi.svg";
+import { ReactComponent as IntroFarhangestan } from "../../assets/svg/dics/farhangestan.svg";
+import { ReactComponent as IntroGanjvar } from "../../assets/svg/dics/ganjvar.svg";
 
 const Icons = {
   FourColumns,
   OneColumn,
   ThreeColumns,
   TwoColumns,
+  IntroMotaradef,
+  IntroSereh,
+  IntroTeyfi,
+  IntroFarhangestan,
+  IntroGanjvar,
 };
 export default Icons;
