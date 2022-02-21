42916cb669c98fd03dda2d5a4d66cd4719a83572
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-02-21 12:40:04 +0330

    Changed current server to sync and started clean server with MongoDB based search

--- server-sync/index.js
+++ server-sync/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 const morgan = require("morgan");
 
-// dictionaries
+// routes
 const emlaei = require("./routes/emlaei");
 const farhangestan = require("./routes/farhangestan");
 const ganjvar = require("./routes/ganjvar");
@@ -12,10 +12,15 @@ const sereh = require("./routes/sereh");
 const teyfi = require("./routes/teyfi");
 
 // middlewares
-app.use(morgan("dev"));
 app.use(cors());
 
-// routes
+// dev only
+if (app.get("env") === "development") {
+  app.use(morgan("dev"));
+  console.log("DEV: Morgan enabled");
+}
+
+// routes middlewares
 app.use("/api/emlaei", emlaei);
 app.use("/api/farhangestan", farhangestan);
 app.use("/api/ganjvar", ganjvar);
