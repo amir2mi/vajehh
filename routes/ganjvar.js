3fe789942c1cd6236d0ce92882cbceb55d951f66
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-02-22 17:58:31 +0330

    Added sanitization and other routes for new server API

--- routes/ganjvar.js
+++ routes/ganjvar.js
@@ -5,15 +5,15 @@ const router = express.Router();
 
 router.get("/:word", async (req, res) => {
   const { word } = req.params;
-  const database = req.app.locals.firstDatabase;
+  const database = req.app.locals.secondDatabase;
 
   // only search if the database is connected
   if (!database) return res.status(500).send("Database is not connected");
 
   try {
-    const result = await searchWord(database, "motaradef", sanitizeText(word));
+    const result = await searchWord(database, "ganjvar", sanitizeText(word), 50, false);
     res.send({
-      kind: "motaradef",
+      kind: "ganjvar",
       items: result,
     });
   } catch (err) {
