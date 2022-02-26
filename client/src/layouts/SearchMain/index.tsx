49b53136b46029fd179f100352ab2cc4672f588b
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-02-26 13:34:48 +0330

    removed title component to use navigation instead

--- client/src/layouts/SearchMain/index.tsx
+++ client/src/layouts/SearchMain/index.tsx
@@ -1,14 +1,12 @@
 import { SearchProvider } from "../../contexts/search";
 import { SettingsProvider } from "../../contexts/settings";
 import { DictionaryProvider } from "../../contexts/dictionary";
-import Title from "../../components/Title";
 import SearchArea from "../../components/SearchArea";
 import ResultTabs from "../../components/ResultTabs";
 
 export default function SearchMain() {
   return (
     <main className="search-main container-lg">
-      <Title id="app-title" title="واژه" subtitle="زیبا و خلاق بنویسید" />
       <SettingsProvider>
         <SearchProvider>
           <DictionaryProvider>
