bf8fa6b42bc83b8c921f80abed6d4ca1dcdfca3e
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-03-08 02:29:19 +0330

    Client - Added halftone pattern for share box and improved responive for header and footer

--- client/src/components/ShareBox/index.tsx
+++ client/src/components/ShareBox/index.tsx
@@ -6,8 +6,9 @@ import "./style.scss";
 export default function ShareBox() {
   return (
     <RotatedBox rotateContent className="share-box">
-      <Info title="اشتراک گذاری" subtitle="به دوستان و آشنایان در مورد واژه بگویید!" />
-      <img className="share-image" src={DaftPunk} alt="اشتراک‌گذاری واژه"  draggable={false}/>
+      <Info title="اشتراک‌‌گذاری" subtitle="به دوستان و آشنایان در مورد واژه بگویید!" />
+      <img className="share-image" src={DaftPunk} alt="اشتراک‌گذاری واژه" draggable={false} />
+      <span className="halftone"></span>
     </RotatedBox>
   );
 }
