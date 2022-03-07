3b000c2f10fcfd8259a1acb8662c012f32c7d64f
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-03-08 02:54:10 +0330

    Client - Optimized halftone pattern styles and added buttons to share box

--- client/src/components/ShareBox/index.tsx
+++ client/src/components/ShareBox/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "react-flatifycss";
 import DaftPunk from "../../assets/images/daft-punk-retro.png";
 import RotatedBox from "../RotatedBox";
 import Info from "../Info";
@@ -6,7 +7,18 @@ import "./style.scss";
 export default function ShareBox() {
   return (
     <RotatedBox rotateContent className="share-box">
-      <Info title="اشتراک‌‌گذاری" subtitle="به دوستان و آشنایان در مورد واژه بگویید!" />
+      <Info
+        title={
+          <>
+            اشتراک‌‌گذاری
+            <div className="share-buttons">
+              <Button>Share</Button>
+              <Button>Tweet</Button>
+            </div>
+          </>
+        }
+        subtitle="به دوستان و آشنایان در مورد واژه خبر دهید!"
+      />
       <img className="share-image" src={DaftPunk} alt="اشتراک‌گذاری واژه" draggable={false} />
       <span className="halftone"></span>
     </RotatedBox>
