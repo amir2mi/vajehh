3fce4b7fcf8d3455f59028c7d00108927b0acbb1
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-03-12 15:17:01 +0330

    Client - Added tests for search area input value changes

--- client/src/layouts/SearchArea/SearchArea.test.tsx
+++ client/src/layouts/SearchArea/SearchArea.test.tsx
@@ -1,15 +1,35 @@
 import { render, screen } from "@testing-library/react";
-import {TestWrapper} from "../../components";
+import userEvent from "@testing-library/user-event";
+import { TestWrapper } from "../../components";
+import config from "../../config.json";
 import SearchArea from ".";
+import { debug } from "console";
 
-describe("Search area", () => {
-  it("should be rendered without crashing", () => {
+describe("<SearchArea/>", () => {
+  const setup = () =>
     render(
       <TestWrapper>
         <SearchArea />
       </TestWrapper>
     );
 
+  it("should be rendered without crashing", () => {
+    setup();
     expect(screen.getByRole("form")).toBeInTheDocument();
+  });
+
+  it("should change the input value on type", async () => {
+    setup();
+    const input = screen.getByRole("textbox");
+    userEvent.type(input, "test");
+    expect(input).toHaveValue("test");
+  });
+
+  it("should change context value automatically after delay", async () => {
+    setup();
+    const input = screen.getByRole("textbox");
+    userEvent.type(input, "test");
+
+    // expect().toHaveTextContent("test");
   });
 });
