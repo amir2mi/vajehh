8532b095ac035ad317d68a58bfbd967a652d4fce
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-03-12 16:43:07 +0330

    Client - Added result tabs test

---
+++ client/src/layouts/ResultTabs/ResultTabs.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import { TestWrapper } from "../../components";
+import ResultTabs from ".";
+
+describe("<ResultTabs/>", () => {
+  const setup = () => {
+    render(
+      <TestWrapper>
+        <ResultTabs />
+      </TestWrapper>
+    );
+  };
+
+  it("should be rendered without crashing", () => {
+    setup();
+    expect(screen.getByRole("tablist")).toBeInTheDocument();
+  });
+});
