3295bdf8e00e8c9921f62b10f2dd15a531c60ef4
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-03-17 00:13:59 +0330

    Reset to 647ba713c407073a13c1b8531450417b4bf1782f

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
+
 const cors = require("cors");
 const morgan = require("morgan");
 const motaradef = require("./routes/motaradef");
@@ -22,6 +23,8 @@ const secondDatabaseClient = new MongoClient(process.env["VAJEHH_SECOND_DB_URL"]
 
 // Middlewares
 const app = express();
+app.use("/cors", anywhere());
+
 app.use(
   cors({
     origin: "https://vajehh.com",
