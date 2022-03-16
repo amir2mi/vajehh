8699eedd5a8fd7a600cc554638eeab8dd629263f
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-03-17 01:19:51 +0330

    Server - Removed motaradef CORS

--- server/routes/motaradef.js
+++ server/routes/motaradef.js
@@ -1,34 +1,26 @@
 const express = require("express");
 const searchWord = require("../searcher");
 const sanitizeText = require("../sanitizer");
-const cors = require("cors");
 const router = express.Router();
 
-router.get(
-  "/:word",
-  cors({
-    origin: "https://vajehh.com",
-    credentials: true,
-  }),
-  async (req, res) => {
-    const { word } = req.params;
-    const { fuzzy } = req.query;
-    const isFuzzy = String(fuzzy).toLowerCase() === "true";
-    const database = req.app.locals.firstDatabase;
+router.get("/:word", async (req, res) => {
+  const { word } = req.params;
+  const { fuzzy } = req.query;
+  const isFuzzy = String(fuzzy).toLowerCase() === "true";
+  const database = req.app.locals.firstDatabase;
 
-    // only search if the database is connected
-    if (!database) return res.status(500).send("Database is not connected");
+  // only search if the database is connected
+  if (!database) return res.status(500).send("Database is not connected");
 
-    try {
-      const result = await searchWord(database, "motaradef", sanitizeText(word), 100, isFuzzy);
-      return res.send({
-        kind: "motaradef",
-        items: result,
-      });
-    } catch (err) {
-      return res.status(500).send(err);
-    }
+  try {
+    const result = await searchWord(database, "motaradef", sanitizeText(word), 100, isFuzzy);
+    return res.send({
+      kind: "motaradef",
+      items: result,
+    });
+  } catch (err) {
+    return res.status(500).send(err);
   }
-);
+});
 
 module.exports = router;
