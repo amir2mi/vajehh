98e6e26ba1afdf13475f6f8bbbde6df490df6a1a
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-03-17 02:13:57 +0330

    Client - Added Origin to API

--- client/src/services/api/index.tsx
+++ client/src/services/api/index.tsx
@@ -5,7 +5,13 @@ import { AllowedDictionaries } from "../../contexts/dictionary";
 export async function searchWord(dic: AllowedDictionaries, word: string, fuzzy?: boolean) {
   // remove all slashes from the given string to prevent server-side errors
   word = word.replace(/\//g, "");
+
   // Use real API when it is in production mode
   const endPoint: string = process.env.NODE_ENV !== "production" ? config.apiEndpointURL__dev : config.apiEndpointURL;
-  return axios.get(`${endPoint}/${dic}/${word}${fuzzy ? "?fuzzy=true" : ""}`,);
+  return axios({
+    method: "get",
+    url: `${endPoint}/${dic}/${word}${fuzzy ? "?fuzzy=true" : ""}`,
+    headers: { Origin: "https://vajehh.herokuapp.com" },
+  });
+  // axios.get(,);
 }
