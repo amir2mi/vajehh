4617efb85a6d951dd3f09e37e08950485c04bf32
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-04-07 20:50:55 +0430

    Client - Added animated class to footer main firefly

--- client/src/layouts/FooterFireflies/index.tsx
+++ client/src/layouts/FooterFireflies/index.tsx
@@ -5,7 +5,7 @@ export default function FooterFireflies() {
   return (
     <div aria-hidden className="footer-fireflies">
       <Firefly className="right" />
-      <Firefly className="center" />
+      <Firefly className="center" animated />
       <Firefly className="left" />
     </div>
   );
