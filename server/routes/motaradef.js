ddad77ba1d1c4fe996bc9392d11e114e9eb57a7f
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-06-28 02:19:14 +0430

    Server - Added dynamic search image for other dictionaries

--- server/routes/motaradef.js
+++ server/routes/motaradef.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const searchWord = require("../searcher");
+const renderSearchImage = require("../utils/render-search-image");
 const router = express.Router();
 
 router.get("/:word", async (req, res) => {
@@ -16,6 +17,38 @@ router.get("/:word", async (req, res) => {
     return res.send({
       kind: "motaradef",
       items: result,
+    });
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+router.get("/image/:word", async (req, res) => {
+  const { word } = req.params;
+  const database = req.app.locals.firstDatabase;
+
+  // only search if the database is connected
+  if (!database) return res.status(500).send("Database is not connected");
+
+  try {
+    const result = await searchWord(database, "motaradef", word, 1, false);
+
+    if (result.length === 0)
+      return res.status(404).send({
+        kind: "motaradef:image",
+        url: "",
+        message: `Could not find any definition related to [${word}] to create the image`,
+      });
+
+    const imageDataUrl = await renderSearchImage(result[0], {
+      primaryColor: "#D392FF",
+      linesCount: 8,
+      linesOrdinalMargin: true,
+    });
+
+    return res.send({
+      kind: "motaradef:image",
+      url: imageDataUrl,
     });
   } catch (err) {
     return res.status(500).send(err);
