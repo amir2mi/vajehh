16c450df1b8d4e80ac721735bef7fedb1333cc60
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-10-28 19:42:08 +0330

    Client - Fixed support amount input conversion to rial

--- client/src/layouts/DonateInfo/DonateInput.tsx
+++ client/src/layouts/DonateInfo/DonateInput.tsx
@@ -1,19 +1,22 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { Input, Button } from "react-flatifycss";
 import config from "../../config.json";
 
 export default function DonateInput() {
   const [amount, setAmount] = useState<string | number | null>(null);
 
-  const handleOnSubmit = (e) => {
+  const handleOnSubmit = (e: FormEvent) => {
     e.preventDefault();
-    window.open(`${config.donateURL}/${amount}`, "_blank");
+    const rialAmount = parseInt(String(amount)) * 10;
+    window.open(`${config.donateURL}/${rialAmount}`, "_blank");
   };
 
   return (
     <form className="input-with-button" onSubmit={handleOnSubmit}>
       <Input required type="number" min="0" placeholder="مبلغ موردنظر به تومان" onChange={(value) => setAmount(value)}>
-        <Button state="static" theme="blue">حمایت</Button>
+        <Button state="static" theme="blue">
+          حمایت
+        </Button>
       </Input>
     </form>
   );
