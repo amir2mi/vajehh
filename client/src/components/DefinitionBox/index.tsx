be3dfed8a7d43533b3fa284a7c82e502e2fa3fab
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-12-05 02:06:58 +0330

    Client - Added images to definition box

--- client/src/components/DefinitionBox/index.tsx
+++ client/src/components/DefinitionBox/index.tsx
@@ -4,6 +4,14 @@ import Highlighter from "react-highlight-words";
 import Icons from "../Icons";
 import "./style.scss";
 
+export interface DefinitionImagesProps {
+  link: string;
+  mime: string;
+  source: string;
+  thumbnail_link: string;
+  title: string;
+}
+
 interface DefinitionBoxProps {
   children?: string | React.ReactNode;
   className?: string;
@@ -11,20 +19,49 @@ interface DefinitionBoxProps {
   hasMultipleLine?: boolean;
   highlight?: string[] | false;
   highlightColor?: string;
+  images?: DefinitionImagesProps[];
   limit?: number | false;
   title: string;
   titleTagName?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 }
 
 export default function DefinitionBox(props: DefinitionBoxProps) {
-  const { children, className, definition, hasMultipleLine, highlight, highlightColor, limit, title, titleTagName } =
-    props;
+  const {
+    children,
+    className,
+    definition,
+    hasMultipleLine,
+    highlight,
+    highlightColor,
+    images,
+    limit,
+    title,
+    titleTagName,
+  } = props;
   const Heading = titleTagName || "h2";
   const [isLimited, setIsLimited] = useState(definition && limit && definition.length > limit);
 
   return (
-    <div className={clsx("definition-box", className && className)}>
-      <Heading className="definition-title">{title}</Heading>
+    <article className={clsx("definition-box", className, !images && "crumbled")}>
+      <header className="main-header">
+        {images?.length && (
+          <button className="images-preview">
+            {images.slice(0, 3).map((image, index) => {
+              return (
+                <img
+                  key={image.title + index}
+                  className="preview"
+                  src={image.thumbnail_link}
+                  alt={image.title}
+                  loading="lazy"
+                />
+              );
+            })}
+          </button>
+        )}
+        <Heading className="definition-title">{title}</Heading>
+      </header>
+
       <p className={clsx("definition-content", isLimited && "limited")}>
         {hasMultipleLine && Array.isArray(definition) ? (
           definition.map((line: string, index) => {
@@ -71,6 +108,6 @@ export default function DefinitionBox(props: DefinitionBoxProps) {
           </button>
         )}
       </p>
-    </div>
+    </article>
   );
 }
