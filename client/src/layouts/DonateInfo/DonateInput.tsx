fa45eaf6a3aa08c55dbb9b581e828dac45c14d79
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2023-02-06 22:31:08 +0330

    Client - Changed donations to Hamibash with Euro and Toman

--- client/src/layouts/DonateInfo/DonateInput.tsx
+++ client/src/layouts/DonateInfo/DonateInput.tsx
@@ -1,23 +1,32 @@
 import { FormEvent, useState } from "react";
-import { Input, Button } from "react-flatifycss";
+import { Input, Button, Checkbox } from "react-flatifycss";
 import config from "../../config.json";
 
 export default function DonateInput() {
   const [amount, setAmount] = useState<string | number | null>(null);
+  const [isEuro, setIsEuro] = useState<boolean>(false);
 
   const handleOnSubmit = (e: FormEvent) => {
     e.preventDefault();
-    const rialAmount = parseInt(String(amount)) * 10;
-    window.open(`${config.donateURL}/${rialAmount}`, "_blank");
+    window.open(`${config.donateURL}/${isEuro ? "euro" : "toman"}/${amount}`, "_blank");
   };
 
   return (
     <form className="input-with-button" onSubmit={handleOnSubmit}>
-      <Input required type="number" min="0" placeholder="مبلغ موردنظر به تومان" onChange={(value) => setAmount(value)}>
+      <Input
+        required
+        type="number"
+        min="0"
+        placeholder={`مبلغ موردنظر به ${isEuro ? "یورو" : "تومان"}`}
+        onChange={(value) => setAmount(value)}
+      >
         <Button state="static" theme="blue">
           حمایت
         </Button>
       </Input>
+      <Checkbox size="sm" checked={isEuro} onClick={() => setIsEuro(!isEuro)} sx={`margin-bottom: 2em`}>
+        خارج ایران هستم (€)
+      </Checkbox>
     </form>
   );
 }
