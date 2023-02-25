9d66b6a1d5fa37d9d29fadbbe14c0859ab32b5d9
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2023-02-26 01:15:39 +0330

    Client - Refactored chat intro

--- client/src/pages/chat/index.tsx
+++ client/src/pages/chat/index.tsx
@@ -1,12 +1,14 @@
 import { MessagesProvider } from "@contexts/messages";
 import MessageInput from "@layouts/Chat/MessageInput/MessageInput";
 import MessagesHistory from "@layouts/Chat/MessagesHistory";
+import MessagesIntro from "@layouts/Chat/MessagesIntro";
 import "./style.scss";
 
 export default function ChatPage() {
   return (
     <main className="chat-main container-lg">
       <MessagesProvider>
+        <MessagesIntro />
         <MessagesHistory />
         <MessageInput />
       </MessagesProvider>
