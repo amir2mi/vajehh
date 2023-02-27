497f4f350718090683b4138f23087dc2efada4e3
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2023-02-27 21:49:35 +0330

    Client - Added document titles for chat page

--- client/src/pages/chat/index.tsx
+++ client/src/pages/chat/index.tsx
@@ -2,9 +2,14 @@ import { MessagesProvider } from "@contexts/messages";
 import MessageInput from "@layouts/Chat/MessageInput/MessageInput";
 import MessagesHistory from "@layouts/Chat/MessagesHistory";
 import MessagesIntro from "@layouts/Chat/MessagesIntro";
+import { useEffect } from "react";
 import "./style.scss";
 
 export default function ChatPage() {
+  useEffect(() => {
+    document.title = "واژه | چت هوشمند ChatGPT - نسخه آنلاین فارسی";
+  }, []);
+
   return (
     <main className="chat-main container-lg">
       <MessagesProvider>
